7bf1496aa191d4ef51581c888e488235e2bd52d0
= <=>
2020-06-22 21:32:39 +0530

    add welcome to java

---
+++ Java/01. Introduction/01. Welcome to Java!/Solution.java
@@ -0,0 +1,11 @@
+// Problem: https://www.hackerrank.com/challenges/welcome-to-java
+// Difficulty: Easy
+// Score: 3
+
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("Hello, World.");
+		System.out.println("Hello, Java.");
+	}
+}
