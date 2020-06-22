da9c976658284aaef2ab40a10c256140b8b3f179
= <=>
2020-06-22 21:34:01 +0530

    add java sub string

---
+++ Java/02. Strings/02. Java Substring/Solution.java
@@ -0,0 +1,17 @@
+// Problem: https://www.hackerrank.com/challenges/java-substring
+// Difficulty: Easy
+// Score: 5
+
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		int start = in.nextInt();
+		int end = in.nextInt();
+		System.out.println(S.substring(start, end));
+		in.close();
+	}
+}
