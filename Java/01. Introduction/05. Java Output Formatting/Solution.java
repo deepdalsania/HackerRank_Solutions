e8e1cfa85d38fd9c357ba7d71039fa03fc165eb9
= <=>
2020-07-05 13:57:45 +0530

    add java output formatting

---
+++ Java/01. Introduction/05. Java Output Formatting/Solution.java
@@ -0,0 +1,21 @@
+// Problem: https://www.hackerrank.com/challenges/java-output-formatting
+// Difficulty: Easy
+// Score: 10
+
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("================================");
+		for (int i = 0; i < 3; i++) {
+			String s1 = sc.next();
+			int x = sc.nextInt();
+			System.err.printf("%-15s%03d\n", s1, x);
+		}
+		System.out.println("================================");
+		sc.close();
+
+	}
+}
